930c7d1e6fe09187b85ce0c64e3e85383c4cbdd0
hoangphihiep <phihiep31012004@gmail.com>
2025-07-08 13:44:22 +0700

    Initial commit: Add backend and frontend

---
+++ backend/backend/src/main/java/hcmute/edu/vn/baiTapCuoiKyKTPM/BackendApplication.java
@@ -0,0 +1,13 @@
+package hcmute.edu.vn.baiTapCuoiKyKTPM;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendApplication.class, args);
+	}
+
+}
---
+++ backend/backend/src/test/java/hcmute/edu/vn/baiTapCuoiKyKTPM/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package hcmute.edu.vn.baiTapCuoiKyKTPM;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
